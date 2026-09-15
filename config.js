5fc494e3d8d6bd2283f35f5adc93ee2d8822cfca
techSBYO <itmsbyo@swiss-belhotel.com>
2026-09-15 12:25:30 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Ganti dengan URL Web App Google Apps Script setelah Deploy.
+// Contoh: https://script.google.com/macros/s/DEPLOYMENT_ID/exec
+window.FLEET_CONFIG = {
+  API_URL: "https://script.google.com/macros/s/AKfycbyQTpVJI_ad1VD77zayfbgvnTSGIdZ9Rexl06Dhw_JbjdfnGkaXay4D_0dWinveenPp/exec"
+};
