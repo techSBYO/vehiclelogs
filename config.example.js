c037d560b1d258d0824e49eb3aa26e8db296953d
techSBYO <itmsbyo@swiss-belhotel.com>
2026-09-15 12:47:23 +0700

    config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,5 @@
 // Ganti dengan URL Web App Google Apps Script setelah Deploy.
 // Contoh: https://script.google.com/macros/s/DEPLOYMENT_ID/exec
 window.FLEET_CONFIG = {
-  API_URL: "https://script.google.com/macros/s/AKfycbyQTpVJI_ad1VD77zayfbgvnTSGIdZ9Rexl06Dhw_JbjdfnGkaXay4D_0dWinveenPp/exec"
+  API_URL: "https://script.google.com/macros/s/AKfycbwlJCkYFx8pjxH9pynmqqIEqs0ikKvV9mSK7MAbOr1NsmSM2PqYIxRt7ppAowoAlqZt/exec"
 };
